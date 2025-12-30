dfbea2ea165c5d2d1613d8119bc39d649dc6787e
arsalan ahmed <arsalanahmeddev1@gmail.com>
2025-12-30 21:06:23 +0000

    backuop databases

--- config/mail.php
+++ config/mail.php
@@ -99,7 +99,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'production8419@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'asjadmmc67@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'FITNEX'),
     ],
 
