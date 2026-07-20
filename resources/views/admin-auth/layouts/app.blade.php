26f0820def59dc9bb39e53f303f82bfb52c8b9c4
Asjad <asjadmmc67@gmail.com>
2026-07-20 21:16:23 +0500

    updated new change comma to .

--- resources/views/admin-auth/layouts/app.blade.php
+++ resources/views/admin-auth/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>@yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
